a6dc98261081cf2f84f429c2d75fc207d2f04309
Gavin <waltersg298@gmail.com>
2022-10-24 18:57:10 -0600

    Now capitlizing first letter of each word in a string (passed tests).

--- js/7thkyu.js
+++ js/7thkyu.js
@@ -6,10 +6,10 @@ function toJadenCase(input) {
     input = input.split(" ");
 
     for (let i = 0; i < input.length; i++) {
-        input[i] = input[i].charAt(0).toUpperCase() + input[i].slice(1);
+        input[i] = input[i][0].toUpperCase() + input[i].substr(1);
     }
 
-    return input;
+    return input.join(" ");
 };
 
 module.exports = { toJadenCase }; 
